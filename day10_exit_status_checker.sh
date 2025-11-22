280a0f1e59934cb879195e95ac7ebaf02760fe4f
abdishakur ahmed <abdishakurahmed.2003@gmail.com>
2025-11-22 13:21:26 +0300

    change to argument you can give instead of hardcoded value for the interpreter to check

--- day10_exit_status_checker.sh
+++ day10_exit_status_checker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #searching for non-existing file to see the exit status of ls
-ls day100_complete.sh
+# give arguments that are invalid file that doesn't exist
+ls "$1"
 
 echo $?
-
-
